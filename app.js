b029764ed2510bad9ed14d859e65a05082079522
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-06 23:52:18 +0600

    err handaling middleware add

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-// middlewares
+// Middleware
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173"], // Adjust as needed
   })
 );
 
 app.get("/", (req, res) => {
   res.send("Server is running");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
 });
 
 app.listen(port, () => {
