dd53e47707e9115917f59f44ccda1adc6b7c58a1
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-06 22:27:56 +0600

    cors origin add

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import "dotenv/config";
-
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
 
 // middlewares
-app.use();
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
