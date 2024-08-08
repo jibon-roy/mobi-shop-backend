1d19cf403ad3b08c55deac764a4c63e1f94ac9d8
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-08 12:38:41 +0600

    cors origin changed

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [process.env.DEV_URL, process.env.PROD_URL, process.env.PROD_URL2],
   })
 );
 
