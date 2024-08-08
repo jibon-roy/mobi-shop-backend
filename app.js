ade6c9381a24455eee31731ed1de2d1cad681965
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-08 15:09:23 +0600

    server status page added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import path from "path";
+import url from "url";
 import connectDB from "./src/config/db/db.js";
 import router from "./src/api/v1/index.js";
+
+// Create __dirname equivalent for ES modules
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,8 +21,15 @@ app.use(
   })
 );
 
-// Define your routes
+// Serve static files (e.g., CSS)
+
+// Define your API routes
 app.use("/api/v1", router);
+
+// Serve the status page
+app.get("/status", (req, res) => {
+  res.sendFile(path.join(__dirname, "/styles", "status.html"));
+});
 
 // Catch-all route for undefined routes
 app.all("*", (req, res) => {
@@ -29,6 +42,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!" });
 });
 
+// Start the server
 const startServer = async () => {
   await connectDB();
   app.listen(port, () => {
